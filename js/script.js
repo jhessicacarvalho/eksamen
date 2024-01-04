93e7476541190ff17f7152119acdb93044d20d0d
jhessicacarvalho <132554850+jhessicacarvalho@users.noreply.github.com>
2024-01-04 13:49:32 +0100

    ændring af burgermenu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", showTitle);
+
+function toggleMenu() {
+    var menuItems = document.querySelector('.menu-items');
+    menuItems.style.display = menuItems.style.display === 'block' ? 'none' : 'block';
+  }
+  
